8ec9daa206bc27db86894e56b0e55ac0fe307bc5
amir <amirjayousi2@gmail.com>
2025-12-20 19:57:59 +0200

    v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Replace 'username' with your GitHub username 
   // and 'repo-name' with your repository name
-  site: 'https://jyyu99.github.io',
-  base: '/netlify-drop-demo-portfolio-main', 
+  site: 'https://jyyu99.github.io', 
 });
